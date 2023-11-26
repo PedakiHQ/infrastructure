62e32dc66d152a7bb1629d7c9ef2d374673830c4
Nathan <nathan.d0601@gmail.com>
2023-11-26 15:29:54 +0100

    ensure ssl on rds (#15)

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -1,3 +1,4 @@
+import { createRdsParameterGroup } from './resources/rds-group';
 import { createStaticBucket } from './resources/static';
 import { createSharedParameters } from './secrets/parameters';
 import { createApiPulumiUser } from './users/api-pulumi';
@@ -5,3 +6,4 @@ import { createApiPulumiUser } from './users/api-pulumi';
 createStaticBucket();
 createApiPulumiUser();
 createSharedParameters();
+createRdsParameterGroup();

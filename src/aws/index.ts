00f2e72cadf8ce3892d5bd44c125b841eb8c6ca6
Vahor <me@vahor.fr>
2023-11-26 12:35:07 +0100

    format

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -1,7 +1,7 @@
+import { createRdsParameterGroup } from './resources/rds-group';
 import { createStaticBucket } from './resources/static';
 import { createSharedParameters } from './secrets/parameters';
 import { createApiPulumiUser } from './users/api-pulumi';
-import {createRdsParameterGroup} from "./resources/rds-group";
 
 createStaticBucket();
 createApiPulumiUser();

49fc79e0463f14d0a9b88dc1d05c3084a451890c
Vahor <me@vahor.fr>
2023-12-25 19:00:11 +0100

    private bucket

--- src/aws/resources/customer-bucket.ts
+++ src/aws/resources/customer-bucket.ts
@@ -3,5 +3,6 @@ import * as aws from '@pulumi/aws';
 export const createFilesBucket = () => {
   const bucket = new aws.s3.Bucket('files.pedaki.fr', {
     bucket: 'files.pedaki.fr',
+    acl: 'private'
   });
 };

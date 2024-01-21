f7f44634c19a32d21fa47c9a12b48b479843dab6
Vahor <me@vahor.fr>
2024-01-21 16:57:34 +0100

    use postgres family

--- src/aws/resources/rds-group.ts
+++ src/aws/resources/rds-group.ts
@@ -3,7 +3,7 @@ import { TAGS } from '../constants';
 
 export const createRdsParameterGroup = () => {
   const group = new aws.rds.ParameterGroup('rds-pedaki', {
-    family: 'mysql8.0',
+    family: 'postgres16',
     description: 'Shared parameter group',
     name: 'rds-pedaki',
     parameters: [{ name: 'require_secure_transport', value: '1' }],
